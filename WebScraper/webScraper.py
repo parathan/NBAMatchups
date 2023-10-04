73f10597136c80dba4ab7c2f8048126eee9e7ba7
Parathan <parathan243@gmail.com>
2023-10-04 00:37:42 -0400

    Seperate table adding into function

--- WebScraper/webScraper.py
+++ WebScraper/webScraper.py
@@ -18,74 +18,31 @@ def webScraper(year: str):
 
     teamStats = []
 
-    soup_teamStats = soup.find(name='table', attrs = {'id': 'per_game-team'})
-    for row in soup_teamStats.find_all('tr')[1:31]:
-
-        # Add Traditional Stats
-        team = {}
-        team['Name'] = row.find('a').text
-        fields = ['g', 'mp', 'fg', 'fga', 'fg_pct', 'fg3', 'fg3a', 'fg3_pct',
-                  'fg2', 'fg2a', 'fg2_pct', 'ft', 'fta', 'ft_pct', 'orb', 'drb',
-                  'trb', 'ast', 'stl', 'blk', 'tov', 'pf', 'pts']
-        for field in fields:
-            team[field] = row.find('td', {'data-stat' : field}).text
-        teamStats.append(team)
+    traditionalFields = ['g', 'mp', 'fg', 'fga', 'fg_pct', 'fg3', 'fg3a', 'fg3_pct',
+                         'fg2', 'fg2a', 'fg2_pct', 'ft', 'fta', 'ft_pct', 'orb', 'drb',
+                         'trb', 'ast', 'stl', 'blk', 'tov', 'pf', 'pts']
+    oppTraditionalFields = ['opp_fg', 'opp_fga', 'opp_fg_pct', 'opp_fg3', 'opp_fg3a', 'opp_fg3_pct',
+                            'opp_fg2', 'opp_fg2a', 'opp_fg2_pct', 'opp_ft', 'opp_fta', 'opp_ft_pct',
+                            'opp_orb', 'opp_drb', 'opp_trb', 'opp_ast', 'opp_stl', 'opp_blk',
+                            'opp_tov', 'opp_pf', 'opp_pts']
+    advancedFields = ['age', 'wins', 'losses', 'wins_pyth', 'losses_pyth', 'mov', 'sos',
+                      'srs', 'off_rtg', 'def_rtg', 'net_rtg', 'pace', 'fta_per_fga_pct',
+                      'fg3a_per_fga_pct', 'ts_pct', 'efg_pct', 'tov_pct', 'orb_pct',
+                      'ft_rate', 'opp_efg_pct', 'opp_tov_pct', 'drb_pct', 'opp_ft_rate']
+    shootingFields = ['avg_dist', 'pct_fga_fg2a', 'pct_fga_00_03', 'pct_fga_03_10', 'pct_fga_10_16',
+                      'pct_fga_16_xx', 'pct_fga_fg3a', 'fg_pct_00_03', 'fg_pct_03_10', 'fg_pct_10_16', 
+                      'fg_pct_16_xx', 'pct_ast_fg2', 'pct_ast_fg3', 'pct_fga_dunk', 'fg_dunk',
+                      'pct_fga_layup', 'fg_layup', 'pct_fg3a_corner', 'fg3_pct_corner']
+    oppShootingFields = ['opp_avg_dist', 'opp_pct_fga_fg2a', 'opp_pct_fga_00_03', 'opp_pct_fga_03_10', 'opp_pct_fga_10_16',
+                         'opp_pct_fga_16_xx', 'opp_pct_fga_fg3a', 'opp_fg_pct_00_03', 'opp_fg_pct_03_10', 'opp_fg_pct_10_16', 
+                         'opp_fg_pct_16_xx', 'opp_pct_ast_fg2', 'opp_pct_ast_fg3', 'opp_pct_fga_dunk', 'opp_fg_dunk',
+                         'opp_pct_fga_layup', 'opp_fg_layup', 'opp_pct_fg3a_corner', 'opp_fg3_pct_corner']
     
-    soup_teamStatsOpp = soup.find(name='table', attrs = {'id': 'per_game-opponent'})
-    for row in soup_teamStatsOpp.find_all('tr')[1:31]:
-        # Finds team object from table based on name found in the advanced table
-        # to add data to the right row
-        name = row.find('a').text
-        index = findIndex(teamStats, name)
-        # Add Opponent Traditional Stats
-        fields = ['opp_fg', 'opp_fga', 'opp_fg_pct', 'opp_fg3', 'opp_fg3a', 'opp_fg3_pct',
-                  'opp_fg2', 'opp_fg2a', 'opp_fg2_pct', 'opp_ft', 'opp_fta', 'opp_ft_pct',
-                  'opp_orb', 'opp_drb', 'opp_trb', 'opp_ast', 'opp_stl', 'opp_blk',
-                  'opp_tov', 'opp_pf', 'opp_pts']
-        for field in fields:
-            teamStats[index][field] = row.find('td', {'data-stat' : field}).text
-        
-
-
-    soup_advancedStats = soup.find(name='table', attrs={'id': 'advanced-team'})
-    for row in soup_advancedStats.find_all('tr')[2:32]:
-        # Finds team object from table based on name found in the advanced table
-        # to add data to the right row
-        name = row.find('a').text
-        index = findIndex(teamStats, name)
-        
-        # Add Advanced Stats
-        fields = ['age', 'wins', 'losses', 'wins_pyth', 'losses_pyth', 'mov', 'sos',
-                  'srs', 'off_rtg', 'def_rtg', 'net_rtg', 'pace', 'fta_per_fga_pct',
-                  'fg3a_per_fga_pct', 'ts_pct', 'efg_pct', 'tov_pct', 'orb_pct',
-                  'ft_rate', 'opp_efg_pct', 'opp_tov_pct', 'drb_pct', 'opp_ft_rate']
-        for field in fields:
-            teamStats[index][field] = row.find('td', {'data-stat' : field}).text
-
-    soup_shootingStats = soup.find(name='table', attrs={'id' : 'shooting-team'})
-    for row in soup_shootingStats.find_all('tr')[2:32]:
-        name = row.find('a').text
-        index = findIndex(teamStats, name)
-        
-        # Add Team Shooting Stats
-        fields = ['avg_dist', 'pct_fga_fg2a', 'pct_fga_00_03', 'pct_fga_03_10', 'pct_fga_10_16',
-                  'pct_fga_16_xx', 'pct_fga_fg3a', 'fg_pct_00_03', 'fg_pct_03_10', 'fg_pct_10_16', 
-                  'fg_pct_16_xx', 'pct_ast_fg2', 'pct_ast_fg3', 'pct_fga_dunk', 'fg_dunk',
-                  'pct_fga_layup', 'fg_layup', 'pct_fg3a_corner', 'fg3_pct_corner']
-        for field in fields:
-            teamStats[index][field] = row.find('td', {'data-stat' : field}).text
-
-    soup_opponentShootingStats = soup.find(name='table', attrs={'id' : 'shooting-opponent'})
-    for row in soup_opponentShootingStats.find_all('tr')[2:32]:
-        name = row.find('a').text
-        index = findIndex(teamStats, name)
-
-        fields = ['opp_avg_dist', 'opp_pct_fga_fg2a', 'opp_pct_fga_00_03', 'opp_pct_fga_03_10', 'opp_pct_fga_10_16',
-                  'opp_pct_fga_16_xx', 'opp_pct_fga_fg3a', 'opp_fg_pct_00_03', 'opp_fg_pct_03_10', 'opp_fg_pct_10_16', 
-                  'opp_fg_pct_16_xx', 'opp_pct_ast_fg2', 'opp_pct_ast_fg3', 'opp_pct_fga_dunk', 'opp_fg_dunk',
-                  'opp_pct_fga_layup', 'opp_fg_layup', 'opp_pct_fg3a_corner', 'opp_fg3_pct_corner']
-        for field in fields:
-            teamStats[index][field] = row.find('td', {'data-stat' : field}).text
+    fieldAdder(soup, 'per_game-team', 1, 31, traditionalFields, False, teamStats)
+    fieldAdder(soup, 'per_game-opponent', 1, 31, oppTraditionalFields, True, teamStats)
+    fieldAdder(soup, 'advanced-team', 2, 32, advancedFields, True, teamStats)
+    fieldAdder(soup, 'shooting-team', 2, 32, shootingFields, True, teamStats)
+    fieldAdder(soup, 'shooting-opponent', 2, 32, oppShootingFields, True, teamStats)
 
     # converts list to dataframe            
     df = pd.DataFrame(teamStats)
@@ -107,5 +64,21 @@ def webScraper(year: str):
         print("Successfully updated NBA Team Stat Data for " + year)
     except Exception as e:
         print(e)
+
+def fieldAdder(soup: BeautifulSoup, tableName: str, start: int, end: int, fields, fieldExists: bool, teamStats: list):
+    soup_stats = soup.find(name='table', attrs = {'id': tableName})
+    for row in soup_stats.find_all('tr')[start:end]:
+        team = {}
+        name = row.find('a').text
+        if not fieldExists:
+            team["Name"] = name
+            for field in fields:
+                team[field] = row.find('td', {'data-stat' : field}).text
+            teamStats.append(team)
+        else:
+            index = findIndex(teamStats, name)
+            for field in fields:
+                teamStats[index][field] = row.find('td', {'data-stat' : field}).text
+
     
 
