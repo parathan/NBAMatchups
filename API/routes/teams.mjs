5cd0059d6fe3b5b1f7e245268dff9fc95eddb86f
Parathan <parathan243@gmail.com>
2023-10-17 02:48:07 -0400

    Add year to post request body

--- API/routes/teams.mjs
+++ API/routes/teams.mjs
@@ -5,16 +5,19 @@ import { ObjectId } from "mongodb";
 const router = express.Router()
 
 router.post("/", async (req, res) => {
-    let tradCollection = tradDb.collection("NbaTeamStats_2023")
-    let meanCollection = meanDb.collection("NbaTeamStatsMean_2023")
-    let stdCollection = stdDb.collection("NbaTeamStatsStd_2023")
-    let zscoreCollection = zscoreDb.collection("NbaTeamStatsZscore_2023")
+    let team1name = req.body.team1;
+    let team2name = req.body.team2;
+    let year = req.body.year;
+
+    let tradCollection = tradDb.collection("NbaTeamStats_" + year)
+    let meanCollection = meanDb.collection("NbaTeamStatsMean_" + year)
+    let stdCollection = stdDb.collection("NbaTeamStatsStd_" + year)
+    let zscoreCollection = zscoreDb.collection("NbaTeamStatsZscore_" + year)
 
     let meanResults = await meanCollection.find({}).toArray();
     let stdResults = await stdCollection.find({}).toArray();
     
-    let team1name = req.body.team1;
-    let team2name = req.body.team2;
+
     let team1 = await tradCollection.find({Name: team1name}).toArray();
     let team2 = await tradCollection.find({Name: team2name}).toArray();
     let team1Zscore = await zscoreCollection.find({Name: team1name}).toArray();
