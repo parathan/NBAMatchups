77889c50d1ddb97c25aea8ce55abea2be23d1409
Parathan <parathan243@gmail.com>
2023-10-18 12:55:07 -0400

    Change database transactions to run concurrently

--- API/controllers/teamController.mjs
+++ API/controllers/teamController.mjs
@@ -24,24 +24,46 @@ const findTwoTeams = async (req, res, next) => {
         let stdCollection = stdDb.collection("NbaTeamStatsStd_" + year)
         let zscoreCollection = zscoreDb.collection("NbaTeamStatsZscore_" + year)
 
-        let meanResults = await meanCollection.find({}).toArray();
-        let stdResults = await stdCollection.find({}).toArray();
+        // let meanResults = await meanCollection.find({}).toArray();
+        // let stdResults = await stdCollection.find({}).toArray();
         
 
-        let team1 = await tradCollection.find({Name: team1name}).toArray();
-        let team2 = await tradCollection.find({Name: team2name}).toArray();
-        let team1Zscore = await zscoreCollection.find({Name: team1name}).toArray();
-        let team2Zscore = await zscoreCollection.find({Name: team2name}).toArray();
+        // let team1 = await tradCollection.find({Name: team1name}).toArray();
+        // let team2 = await tradCollection.find({Name: team2name}).toArray();
+        // let team1Zscore = await zscoreCollection.find({Name: team1name}).toArray();
+        // let team2Zscore = await zscoreCollection.find({Name: team2name}).toArray();
 
-        let results = {
-            team1Traditional: team1,
-            team2Traditional: team2,
-            mean: meanResults,
-            std: stdResults,
-            team1Zscore: team1Zscore,
-            team2Zscore: team2Zscore
+        // Promise.all runs all promises concurrently.
+        let data = await Promise.all(
+            [
+                meanCollection.find({}).toArray(),
+                stdCollection.find({}).toArray(),
+                tradCollection.find({Name: team1name}).toArray(),
+                tradCollection.find({Name: team2name}).toArray(),
+                zscoreCollection.find({Name: team1name}).toArray(),
+                zscoreCollection.find({Name: team2name}).toArray()
+            ]
+        )
+
+        // let results = {
+        //     team1Traditional: team1,
+        //     team2Traditional: team2,
+        //     mean: meanResults,
+        //     std: stdResults,
+        //     team1Zscore: team1Zscore,
+        //     team2Zscore: team2Zscore
+        // }
+
+        let concurResults = {
+            team1Traditional: data[0],
+            team2Traditional: data[1],
+            mean: data[2],
+            std: data[3],
+            team1Zscore: data[4],
+            team2Zscore: data[5]
         }
-        return res.status(200).json(results)
+
+        return res.status(200).json(concurResults)
     } catch (err) {
         next(err);
     }
