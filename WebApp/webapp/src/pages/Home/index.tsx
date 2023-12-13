924f1e526aca7afd096a30a1cfafc9095ef3f362
Parathan <parathan243@gmail.com>
2023-12-13 13:55:34 -0500

    Make First edit to client

---
+++ WebApp/webapp/src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.module.css';
+
+function Home() {
+  return (
+    <div className="defaultPage">
+      <header className="App-header">
+        <p className='title'>
+          Welcome to NBAMatchups
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
