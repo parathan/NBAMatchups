136ae24a01281ee0caa4a76d90b8ee5c6e8ab407
Parathan <parathan243@gmail.com>
2024-03-09 15:04:40 -0500

    Add link function to navbar

--- WebApp/webapp/src/components/NavBar/index.tsx
+++ WebApp/webapp/src/components/NavBar/index.tsx
@@ -2,6 +2,8 @@ import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import { Box } from "@mui/material";
 import styles from './index.module.css'
 
+import { Link } from "react-router-dom"
+
 function NavBar() {
     return (
         // https://codesandbox.io/p/sandbox/react-dashboard-pnm6fh?file=%2Fsrc%2Fpages%2Fglobal%2Fsidebar%2FMyProSidebar.jsx%3A51%2C6-51%2C9
@@ -26,9 +28,9 @@ function NavBar() {
 
         <Sidebar>
             <Menu>
-                <MenuItem>Home</MenuItem>
-                <MenuItem>Team Matchups</MenuItem>
-                <MenuItem>Win/Loss Prediction</MenuItem>
+                <MenuItem component={<Link to="/" />}>Home</MenuItem>
+                <MenuItem component={<Link to="/matchups" />}>Team Matchups</MenuItem>
+                <MenuItem component={<Link to="/predictions" />}>Win/Loss Prediction</MenuItem>
             </Menu>
         </Sidebar>
     )
--- WebApp/webapp/src/pages/Matchups/index.tsx
+++ WebApp/webapp/src/pages/Matchups/index.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import styles from './index.module.css';
+import Layout from '../../components/Layout/Layout';
 
 function Matchups() {
   return (
-    <div className={styles.homePage}>
-      <header className={styles.header}>
-        <p>
-          Welcome to NBAMatchups
-        </p>
-      </header>
-      <header className={styles.features}>
-        <p>
-          Team Matchups
-        </p>
-        <p>
-          Win/Loss Prediction
-        </p>
-      </header>
-    </div>
+    <Layout>
+      <div className={styles.homePage}>
+        <header className={styles.header}>
+          <p>
+            Welcome to NBAMatchups
+          </p>
+        </header>
+        <header className={styles.features}>
+          <p>
+            Team Matchups
+          </p>
+          <p>
+            Win/Loss Prediction
+          </p>
+        </header>
+      </div>
+    </Layout>
   );
 }
 
