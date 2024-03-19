65d084cd9a881f0c864be08ae9f778768f72c362
Parathan <parathan243@gmail.com>
2024-03-19 03:06:53 -0400

    Create basic layout for matchups page

--- WebApp/webapp/src/pages/Matchups/index.tsx
+++ WebApp/webapp/src/pages/Matchups/index.tsx
@@ -2,20 +2,44 @@ import React from 'react';
 import styles from './index.module.css';
 import Layout from '../../components/Layout/Layout';
 
+import { teamsNames } from '../../constants/teamNames';
+import { years } from '../../constants/years';
+
 function Matchups() {
   return (
     <Layout>
-      <div className={styles.homePage}>
-        <header className={styles.header}>
+      <div className={styles.matchups}>
+        <div className={styles.header}>
           <p>
             Team Matchups
           </p>
-        </header>
-        <header className={styles.features}>
+        </div>
+        <div className={styles.description}>
           <p>
-            This is the Team Matchups feature
+            This is the Team Matchups feature <br/>
+            Please input both teams that you want to compare and the year that you want to use 
           </p>
-        </header>
+        </div>
+        <div className={styles.input}>
+          <select>
+            {teamsNames.map( teamName =>
+              <option value={teamName}>{teamName}</option>
+            )};
+          </select>
+          <select>
+            {teamsNames.map( teamName =>
+              <option value={teamName}>{teamName}</option>
+            )};
+          </select>
+          <select>
+            {years.map( year =>
+              <option value={year}>{year}</option>
+            )};
+          </select>
+        </div>
+        <div>
+          <button>Check Matchup</button>
+        </div>
       </div>
     </Layout>
   );
