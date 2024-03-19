d99d0cedf5c4d39ee5c0d0574b47680ab468b0cf
Parathan <parathan243@gmail.com>
2024-03-19 13:32:04 -0400

    Add axios post request

--- WebApp/webapp/src/pages/Matchups/index.tsx
+++ WebApp/webapp/src/pages/Matchups/index.tsx
@@ -4,15 +4,37 @@ import Layout from '../../components/Layout/Layout';
 
 import { teamsNames } from '../../constants/teamNames';
 import { years } from '../../constants/years';
+import axios from 'axios';
 
 function Matchups() {
 
   const [formVisible, setFormVisible] = useState(true)
   const [progressVisible, setProgressVisible] = useState(false)
+  const [errorVisible, setErrorVisible] = useState(false)
+  const [successVisible, setSuccessVisible] = useState(false)
 
   function onSubmit() {
     setFormVisible(false)
     setProgressVisible(true)
+    getData()
+  }
+
+  function getData() {
+    axios.post('http://localhost:5050/teams/OrderedPercentile', {
+      team1: "Sacramento Kings",
+      team2: "Miami Heat",
+      year: "2023"
+    })
+    .then((response) => {
+      console.log(response)
+      setProgressVisible(false)
+      setSuccessVisible(true)
+    })
+    .catch((error) => {
+      console.log(error)
+      setProgressVisible(false)
+      setErrorVisible(true)
+    })
   }
 
   return (
@@ -53,6 +75,12 @@ function Matchups() {
         </div>
         <div className={`${styles.description} ${progressVisible ? styles.notHidden : styles.hidden}`}>
           In Progress
+        </div>
+        <div className={`${styles.description} ${errorVisible ? styles.notHidden : styles.hidden}`}>
+          An Err occured
+        </div>
+        <div className={`${styles.description} ${successVisible ? styles.notHidden : styles.hidden}`}>
+          Success
         </div>
       </div>
     </Layout>
