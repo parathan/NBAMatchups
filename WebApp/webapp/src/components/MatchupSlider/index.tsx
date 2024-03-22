a10332bad91d59e96c8282dea104a9c8271c0b56
Parathan <parathan243@gmail.com>
2024-03-22 13:21:04 -0400

    Add colouring to slider green-red gradient

--- WebApp/webapp/src/components/MatchupSlider/index.tsx
+++ WebApp/webapp/src/components/MatchupSlider/index.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, useState } from 'react';
 import styles from './index.module.css';
-import { Grid, Slider } from '@mui/material';
+import { Grid, Slider, ThemeProvider, createMuiTheme, createTheme, makeStyles } from '@mui/material';
 
 import { MatchupData } from '../../interfaces/data';
 
@@ -16,6 +16,35 @@ function MatchupSlider(props: MatchupData) {
     const [trad1, setTrad1] = useState(props.team1Percentile1 < props.team2Percentile_Op ? props.team1Trad : props.team2Trad_Op)
     const [trad2, setTrad2] = useState(props.team1Percentile1 >= props.team2Percentile_Op ? props.team1Trad : props.team2Trad_Op)
 
+    function perc2color(perc: number,min: number,max: number) {
+        var base = (max - min);
+
+        if (base == 0) { perc = 100; }
+        else {
+            perc = (perc - min) / base * 100; 
+        }
+        var r, g, b = 0;
+        if (perc < 50) {
+            r = 255;
+            g = Math.round(5.1 * perc);
+        }
+        else {
+            g = 255;
+            r = Math.round(510 - 5.10 * perc);
+        }
+        var h = r * 0x10000 + g * 0x100 + b * 0x1;
+        return '#' + ('000000' + h.toString(16)).slice(-6);
+    }
+
+
+    const customTheme = createTheme({
+        palette: {
+            primary: {
+                main: perc2color(props.absPercentileDifference * 100, 0, 100),
+            }
+        }
+    })
+
     return (
         <Grid container spacing={2} className={styles.slider}>
             <Grid item xs={3} className={styles.description}>
@@ -23,14 +52,15 @@ function MatchupSlider(props: MatchupData) {
                 {trad1}
             </Grid>
             <Grid item xs={6}>
-                <Slider
-                min={0}
-                max={1}
-                value={value}
-                step={0.001}
-                valueLabelDisplay='on'
-                disabled
-                />
+                <ThemeProvider theme={customTheme}>
+                    <Slider
+                        min={0}
+                        max={1}
+                        value={value}
+                        step={0.001}
+                        valueLabelDisplay='on'
+                    />
+                </ThemeProvider>
             </Grid>
             <Grid item xs={3} className={styles.description}>
                 {field2}<br/>
