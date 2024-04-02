700f042219550ba2bbf2133491a58a8c0aa8ad48
Parathan <parathan243@gmail.com>
2024-04-01 22:27:40 -0400

    Create interface for allteams api and create api call in dashboard

--- WebApp/webapp/src/pages/Dashboard/index.tsx
+++ WebApp/webapp/src/pages/Dashboard/index.tsx
@@ -2,8 +2,32 @@ import React, { ChangeEvent, useEffect, useState } from 'react';
 import styles from './index.module.css';
 import Layout from "../../components/Layout/Layout";
 
+import axios from 'axios';
+import { totalTeamData } from '../../interfaces/TotalTeamData';
+
 
 function Dashboard() {
+
+    const [data, setData] = useState<totalTeamData[]>([])
+
+    // Intended that useEffect runs once, as empty array given means that it only updates once when page renders.
+    // In development, it will run twice due to strict mode being on. This won't happen in production, however.
+    // https://byby.dev/useeffect-run-twice
+    useEffect(() => {
+        axios.post(process.env.REACT_APP_API_URL + '/teams/allTeams', {
+            startYear: 2019,
+            endYear: 2023
+        })
+        .then((response) => {
+            console.log(response)
+            setData(response.data)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }, [])
+
+
     return (
         <Layout>
             <div className={styles.header}>Dashboard</div>
