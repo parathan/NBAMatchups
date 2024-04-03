83b3f7a6392176ff2a2210ab07c8f55cb4ace53c
Parathan <parathan243@gmail.com>
2024-04-02 20:47:59 -0400

    Add error and progress sections

--- WebApp/webapp/src/pages/Dashboard/index.tsx
+++ WebApp/webapp/src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import styles from './index.module.css';
 import Layout from "../../components/Layout/Layout";
+import { CircularProgress, Grid } from '@mui/material';
 
 import axios from 'axios';
 import { totalTeamData } from '../../interfaces/TotalTeamData';
@@ -9,6 +10,8 @@ import { totalTeamData } from '../../interfaces/TotalTeamData';
 function Dashboard() {
 
     const [data, setData] = useState<totalTeamData[]>([])
+    const [progress, setProgress] = useState(true)
+    const [error, setError] = useState(false)
 
     // Intended that useEffect runs once, as empty array given means that it only updates once when page renders.
     // In development, it will run twice due to strict mode being on. This won't happen in production, however.
@@ -21,9 +24,12 @@ function Dashboard() {
         .then((response) => {
             console.log(response)
             setData(response.data)
+            setProgress(false)
         })
         .catch((error) => {
             console.log(error)
+            setProgress(false)
+            setError(true)
         })
     }, [])
 
@@ -31,6 +37,12 @@ function Dashboard() {
     return (
         <Layout>
             <div className={styles.header}>Dashboard</div>
+            <div className={`${progress ? styles.notHidden : styles.hidden}`}>
+                <CircularProgress />
+            </div>
+            <div className={`${error ? styles.notHidden : styles.hidden}`}>
+                Errors existing
+            </div>
         </Layout>
     )
 }
