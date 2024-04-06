aa6a0728b7612f3d8c5f5fca6b47f8d6ab82e2ae
Parathan Vakeesan <parathan243@gmail.com>
2024-04-06 15:05:44 -0400

    Initial testing for dashboard component

--- WebApp/webapp/src/pages/Dashboard/index.test.tsx
+++ WebApp/webapp/src/pages/Dashboard/index.test.tsx
@@ -3,15 +3,16 @@ import { render } from '@testing-library/react'
 import Dashboard from '.'
 import { BrowserRouter } from 'react-router-dom'
 
-jest.mock('react-chartjs-2', () => ({ Line: () => null }))
+jest.mock('react-chartjs-2', () => ({ Line: () => null })) // mocks chart in dashboard
+// jest.mock('axios');
 
 describe('Dashboard Component', () => {
     it('Dashboard renders', () => {
         render(
-        <BrowserRouter>
-            <Dashboard />
-        </BrowserRouter>
-        )
+            <BrowserRouter>
+                <Dashboard />
+            </BrowserRouter>
+        );
         // const component = renderer.create(
         //     <BrowserRouter>
         //         <Dashboard/>
@@ -20,4 +21,13 @@ describe('Dashboard Component', () => {
         // let tree = component.toJSON();
         // expect(tree).toMatchSnapshot();
     })
+    // it('renders team and field dropdowns', () => {
+    //     const { getByText } = render(
+    //         <BrowserRouter>
+    //             <Dashboard />
+    //         </BrowserRouter>
+    //     );
+    //     expect(getByText('Pick a Team')).toBeInTheDocument();
+    //     expect(getByText('Pick a Field')).toBeInTheDocument();
+    // });
 })
