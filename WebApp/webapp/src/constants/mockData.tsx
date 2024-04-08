61c78eb3453744d18af69b8961541a656e0a049b
Parathan <parathan243@gmail.com>
2024-04-08 16:16:41 -0400

    refactor tests to elminate mock axios error

---
+++ WebApp/webapp/src/constants/mockData.tsx
@@ -0,0 +1,4 @@
+import { totalTeamData } from "../interfaces/TotalTeamData"
+import _mockDataJson from './mockDataJson.json'
+
+export const mockData: totalTeamData[] = _mockDataJson as totalTeamData[]
--- WebApp/webapp/src/pages/Dashboard/index.test.tsx
+++ WebApp/webapp/src/pages/Dashboard/index.test.tsx
@@ -1,51 +1,115 @@
 // import renderer from 'react-test-renderer'
-import { render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
 import Dashboard from '.'
 import { BrowserRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
 import axios from 'axios'
 import { statsArray } from '../../constants/statArray'
+import { mockData } from '../../constants/mockData'
 
 jest.mock('react-chartjs-2', () => ({ Line: () => null })) // mocks chart in dashboard
-// jest.mock('axios');
+jest.mock('axios', () => ({
+    post: jest.fn()
+}));
 
 
 describe('Dashboard Component', () => {
-    it('Dashboard renders', () => {
-        render(
-            <BrowserRouter>
-                <Dashboard/>
-            </BrowserRouter>
-        )
-    })
-    it('renders team and field dropdowns', () => {
-        render(
-            <BrowserRouter>
-                <Dashboard />
-            </BrowserRouter>
-        );
-        const team = screen.getByText(/Pick a Team/i);
-        expect(team).toBeInTheDocument();
 
-        const field = screen.getByText(/Pick a Field/i);
-        expect(field).toBeInTheDocument();
-    })
-    it('correct number of team dropdowns exist', () => {
+    beforeEach(() => {
+        (axios.post as jest.Mock).mockResolvedValue({
+            data: mockData
+        });
+    });
+
+    it('renders dashboard header', async () => {
         render(
             <BrowserRouter>
                 <Dashboard />
             </BrowserRouter>
         );
-        const options = screen.getAllByTestId("team-options");
-        expect(options.length).toBe(30); //30 teams
-    })
-    it('correct number of field dropdowns exist', () => {
+        const headerArray = await screen.findAllByText(/Dashboard/i);
+        expect(headerArray.length).toBe(2); // One in the page and one in navbar
+    });
+
+    it('renders team dropdown', async () => {
         render(
             <BrowserRouter>
                 <Dashboard />
             </BrowserRouter>
-        )
-        const options = screen.getAllByTestId('field-options');
-        expect(options.length).toBe(statsArray.length)
+        );
+        const teamDropdown = await screen.findByText(/Pick a Team/i);
+        expect(teamDropdown).toBeInTheDocument();
+    });
+
+    it('renders field dropdown', async () => {
+        render(
+            <BrowserRouter>
+                <Dashboard />
+            </BrowserRouter>
+        );
+        const fieldDropdown = await screen.findByText(/Pick a Field/i);
+        expect(fieldDropdown).toBeInTheDocument();
+    });
+
+    it('correct number of team dropdowns exist', async () => {
+        render(
+            <BrowserRouter>
+                <Dashboard />
+            </BrowserRouter>
+        );
+
+        const teamDropdowns = await screen.findAllByTestId('team-options')
+        expect(teamDropdowns.length).toBe(30); // 30 teams
     })
+
+    it('correct number of field dropdowns exist', async () => {
+        render(
+            <BrowserRouter>
+                <Dashboard />
+            </BrowserRouter>
+        );
+
+        const fieldDropdowns = await screen.findAllByTestId('field-options');
+        expect(fieldDropdowns.length).toBe(statsArray.length)
+    })
+
+    // it('Dashboard renders', () => {
+    //     render(
+    //         <BrowserRouter>
+    //             <Dashboard/>
+    //         </BrowserRouter>
+    //     )
+    // })
+    // it('renders team and field dropdowns', async () => {
+    //     mockedAxios.post.mockResolvedValue({ data: mockData });
+
+    //     render(
+    //         <BrowserRouter>
+    //             <Dashboard />
+    //         </BrowserRouter>
+    //     );
+    //     const stringTeam = await screen.findByText(/Pick a Team/i);
+    //     expect(stringTeam).toBeInTheDocument();
+
+    //     const stringField = await screen.findByText(/Pick a Field/i);
+    //     expect(stringField).toBeInTheDocument();
+    // })
+    // it('correct number of team dropdowns exist', () => {
+    //     render(
+    //         <BrowserRouter>
+    //             <Dashboard />
+    //         </BrowserRouter>
+    //     );
+    //     const options = screen.getAllByTestId("team-options");
+    //     expect(options.length).toBe(30); //30 teams
+    // })
+    // it('correct number of field dropdowns exist', () => {
+    //     render(
+    //         <BrowserRouter>
+    //             <Dashboard />
+    //         </BrowserRouter>
+    //     )
+    //     const options = screen.getAllByTestId('field-options');
+    //     expect(options.length).toBe(statsArray.length)
+    // })
 })
