ebe7411461c86956f2d9073b6b3ba197bf26ecda
Parathan <parathan243@gmail.com>
2024-04-11 16:44:30 -0400

    Add documentation for the layout component

--- WebApp/webapp/src/components/Layout/Layout.tsx
+++ WebApp/webapp/src/components/Layout/Layout.tsx
@@ -3,7 +3,12 @@ import styles from './index.module.css';
 
 import NavBar from "../NavBar"
 
-
+/**
+ * The layout component is used to integrate the given children component with the navbar to create pages that have the content
+ * of the chidlren component and the navigation provided by the navbar.
+ * @param children
+ * @returns 
+ */
 function Layout({ children }: {
     children: ReactNode
 }) {
