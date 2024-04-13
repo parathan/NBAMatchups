68dd168452f14129ad1a52f53880df20b6ebcdb9
Parathan <parathan243@gmail.com>
2024-04-12 22:59:00 -0400

    update documentation

--- WebApp/webapp/src/pages/Unknown/index.tsx
+++ WebApp/webapp/src/pages/Unknown/index.tsx
@@ -2,6 +2,10 @@ import { useNavigate } from "react-router-dom";
 import Layout from "../../components/Layout/Layout";
 import styles from './index.module.css';
 
+/**
+ * The Unknown component returns a page that indicates that the user visited a page that does not exist.
+ * It will then redirect the user to the home page. It's purpose is for navigation for unknown routes.
+ */
 function Unknown() {
 
     const navigate = useNavigate();
