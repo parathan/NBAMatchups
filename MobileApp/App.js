f5b684bb5b9563ea7987066fac7d0c5485d73d08
Parathan <parathan243@gmail.com>
2024-05-02 00:21:27 -0400

    reorganize file structure

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HomeScreen from './src/screens/Home/HomeScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
