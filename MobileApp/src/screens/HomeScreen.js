5d3dff18c471d70b41c2fb2adc95c3b54e158164
Parathan <parathan243@gmail.com>
2024-05-02 01:25:03 -0400

    Create initial text and button elements

--- MobileApp/src/screens/HomeScreen.js
+++ MobileApp/src/screens/HomeScreen.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+            <Text style={styles.header}>Welcome to NBAMatchups</Text>
+            <Text style={styles.text}>
+                Jump into Matchups, and compare teams based on their opposing stats.
+            </Text>
+            <Button title='Go to Team Matchups'/>
+            <Text style={styles.text}>
+                Explore our predictive feature, that predicts the winner of a given nba game
+            </Text>
+            <Button title="Go to Win/Loss Prediction" />
+            <Text style={styles.text}>
+                Dig into Dashboard, allowing you to see trends of a teams stats for the past several years
+            </Text>
+            <Button title="Go to Dashboard" />
             <StatusBar style="auto" />
         </View>
     );
@@ -13,8 +25,17 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      backgroundColor: '#222222',
+    //   alignItems: 'center',
+    //   justifyContent: 'center',
     },
+    header: {
+        marginTop: '10%',
+        textAlign: 'center',
+        color: '#ffffff',
+        fontSize: '30sp', //TODO #10
+    },
+    text: {
+        color: '#dddddd',
+    }
 });
