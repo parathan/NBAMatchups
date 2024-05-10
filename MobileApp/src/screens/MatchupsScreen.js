90e595e7d3e2e0273e67d7fb029342c545dc7d11
Parathan <parathan243@gmail.com>
2024-05-10 11:45:48 -0400

    create basic templates for other pages

---
+++ MobileApp/src/screens/MatchupsScreen.js
@@ -0,0 +1,26 @@
+import { View } from "react-native";
+import { StyleSheet, Text } from "react-native";
+
+export default function MatchupsScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>Matchups Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#222222',
+      paddingHorizontal: '10%',
+    //   alignItems: 'center',
+    //   justifyContent: 'center',
+    },
+    header: {
+        marginTop: '10%',
+        textAlign: 'center',
+        color: '#ffffff',
+        fontSize: 30, //TODO #10
+    },
+})
