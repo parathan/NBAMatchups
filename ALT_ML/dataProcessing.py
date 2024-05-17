896c837a44a9ae1e08a94e0c52f426e69c6398f1
Parathan <parathan243@gmail.com>
2024-05-17 09:22:39 -0400

    Finalize data to csv file method

--- ALT_ML/dataProcessing.py
+++ ALT_ML/dataProcessing.py
@@ -1,10 +1,12 @@
 import requests
+import csv
 
 
 # Take data from the api route getallteams and place in the csv file
 # Api needs to be running
 def getAllTeamData(year: int):
     
+    # Get Data from endpoint
     apiEndpoint = 'http://localhost:5050/teams/allTeams'
     data = { 
         'startYear': year,
@@ -12,7 +14,32 @@ def getAllTeamData(year: int):
     }
 
     response = requests.post(url= apiEndpoint, json=data)
-    print(response.text)
+
+    # Put Data in list of dictionaries
+    dataDict = []
+    for team in response.json():
+        if team['teamName'] == 'MEAN':
+            continue
+        
+        dictAdder = {}
+        dictAdder['teamName'] = team['teamName']
+        
+        stats = team['stats'][0]['yearStats']
+        for key, value in stats.items():
+            if key == '_id':
+                continue
+            dictAdder[key] = value
+
+        dataDict.append(dictAdder)
+
+    keys = dataDict[0].keys()
+
+    # Write dictionary to csv file
+    with open('2022-23_TeamAverage.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(dataDict)
+
 
 
 
