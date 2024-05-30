23121b99a2e4df977fbf4be3353a51abe54b9073
Parathan <parathan243@gmail.com>
2024-05-30 14:09:52 -0400

    fix tests for prediction page

--- WebApp/webapp/src/pages/Prediction/index.test.tsx
+++ WebApp/webapp/src/pages/Prediction/index.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import Matchups from '.'
+import Prediction from '.'
 import { MemoryRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
 import axios from 'axios'
@@ -20,47 +20,37 @@ describe("Matchups Page elements render correctly", () => {
     it('renders matchups header', async () => {
         render(
             <MemoryRouter>
-                <Matchups />
+                <Prediction />
             </MemoryRouter>
         )
-        const header = await screen.findAllByText(/Team Matchups/i);
-        expect(header.length).toBe(3);
+        const header = await screen.findAllByText(/Match Prediction/i);
+        expect(header.length).toBe(1);
     })
 
     it('renders team dropdowns', async () => {
         render(
             <MemoryRouter>
-                <Matchups />
+                <Prediction />
             </MemoryRouter>
         )
         const teamDropdowns = await screen.findAllByText(/Pick a Team/i);
         expect(teamDropdowns.length).toBe(2);
     })
 
-    it('renders year dropdown', async () => {
+    it('renders predict winners button', async () => {
         render(
             <MemoryRouter>
-                <Matchups />
+                <Prediction />
             </MemoryRouter>
         )
-        const yearDropdown = await screen.findByText(/Pick a Year/i);
-        expect(yearDropdown).toBeInTheDocument();
-    })
-
-    it('renders check matchups button', async () => {
-        render(
-            <MemoryRouter>
-                <Matchups />
-            </MemoryRouter>
-        )
-        const matchupButton = await screen.findByText(/Check Matchup/i);
+        const matchupButton = await screen.findByText(/Predict Winner/i);
         expect(matchupButton).toBeInTheDocument();
     })
 
     it('correct number of team dropdowns exist for team1', async () => {
         render(
             <MemoryRouter>
-                <Matchups />
+                <Prediction />
             </MemoryRouter>
         )
         const team1Dropdowns = await screen.findAllByTestId('team1-options');
@@ -70,21 +60,11 @@ describe("Matchups Page elements render correctly", () => {
     it('correct number of team dropdowns exist for team2', async () => {
         render(
             <MemoryRouter>
-                <Matchups />
+                <Prediction />
             </MemoryRouter>
         )
         const team2Dropdowns = await screen.findAllByTestId('team2-options');
         expect(team2Dropdowns.length).toBe(30);
-    })
-
-    it('correct number of year dropdowns exist for team2', async () => {
-        render(
-            <MemoryRouter>
-                <Matchups />
-            </MemoryRouter>
-        )
-        const yearDropdowns = await screen.findAllByTestId('year-options');
-        expect(yearDropdowns.length).toBe(5);
     })
 
 })
