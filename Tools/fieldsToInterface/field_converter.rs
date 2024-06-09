9655ebc44dc28b5786c816f93090722096de0ca6
Parathan <parathan243@gmail.com>
2024-06-08 22:08:44 -0400

    Change script to convert text file to vector of strings

--- Tools/fieldsToInterface/field_converter.rs
+++ Tools/fieldsToInterface/field_converter.rs
@@ -2,7 +2,15 @@ use std::fs;
 use std::io;
 
 fn main() -> io::Result<()> {
-    let file_contents = fs::read_to_string("./fields.txt")?;
-    println!("Content: \n{}", file_contents);
+    let mut line_vector = Vec::new();
+
+    let binding = fs::read_to_string("./fields.txt").unwrap();
+    let file_contents = binding.lines();
+    for line in file_contents{
+        line_vector.push(line.to_string());
+    }
+    for line in line_vector {
+        println!("{}", line);
+    }
     Ok(())
 }
