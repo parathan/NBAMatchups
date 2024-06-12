ea2c662ad56606b538e64b635d385ad1ef9e823d
Parathan <parathan243@gmail.com>
2024-06-11 20:52:13 -0400

    finalize script that converts file

--- Tools/statsTexttoArray/json_to_array.rs
+++ Tools/statsTexttoArray/json_to_array.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::io;
+use std::io::prelude::*;
 
 fn main() -> io::Result<()> {
     let mut line_vector = Vec::new();
@@ -14,8 +15,16 @@ fn main() -> io::Result<()> {
     let mut result_string: String = "".to_string();
 
     for line in line_vector {
-        println!("{}", line);
+        let str_vector: Vec<&str> = line.split(":").collect();
+        let first: String = str_vector[0].to_string();
+        let second: String = str_vector[1].trim_matches(' ').to_string();
+        let mut result: String = "['".to_string(); 
+        result = result + &first + "','" + &second + "'],\n";
+        result_string = result_string + &result;
     }
+
+    let ouput_file = fs::File::create("./resultFile_rust.txt");
+    let _ = ouput_file?.write_all(result_string.as_bytes());
 
     Ok(())
 }
