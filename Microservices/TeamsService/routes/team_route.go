f723b42bff6cbe80222383c8dba6bae460523aa1
Parathan <parathan243@gmail.com>
2024-06-18 20:47:16 -0400

    document the routes

--- Microservices/TeamsService/routes/team_route.go
+++ Microservices/TeamsService/routes/team_route.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TeamRoute(app *fiber.App) {
-	app.Post("/teams/all", controllers.GetAllTeams)
-	app.Post("/teams/two", controllers.GetTwoTeams)
-	app.Post("/teams/two/ml", controllers.GetTwoTeamsML)
+	app.Post("/teams/all", controllers.GetAllTeams) // for dashboard feature
+	app.Post("/teams/two", controllers.GetTwoTeams) // for use in matchups feature or prediction feature, simply returns two teams data
+	app.Post("/teams/two/ml", controllers.GetTwoTeamsML) // for use in prediction feature with data already processed
 }
