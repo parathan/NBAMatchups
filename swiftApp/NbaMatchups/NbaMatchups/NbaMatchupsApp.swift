086c39248ded9d9ded01465c70bd0d3bf671f70e
Jaakulan Subeethakumar <jaakulans@hotmail.com>
2024-06-26 19:13:02 -0400

    Created template for app

---
+++ swiftApp/NbaMatchups/NbaMatchups/NbaMatchupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NbaMatchupsApp.swift
+//  NbaMatchups
+//
+//  Created by Jaakulan S on 2024-06-26.
+//
+
+import SwiftUI
+
+@main
+struct NbaMatchupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
