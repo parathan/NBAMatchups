86f6d4d5f5fd91c95db0371aabe726fda8025859
Parathan <parathan243@gmail.com>
2024-07-01 14:10:40 -0400

    remove unneeded functions

--- ETL/businessLogic.py
+++ ETL/businessLogic.py
@@ -43,47 +43,7 @@ def calcData(year: str):
     col = df.pop('Name')
     # writeStatsToDb('RankPercentile', calcRank(df, col), year)
     writeStatsToDb('Percentile', calcPercentile(df, col), year)
-    writeStatsToDb('Zscore', calcZscore(df, col), year)
     writeStatsToDb('Mean', calcMean(df), year)
-    writeStatsToDb('Std', calcStd(df), year)
-
-# Function does not provide meaningful improvement in performance but is here for future reference/improvement
-def calcDataConcurrently(year: str):
-    """Scrape data and calculate statistics for it concurrenty and write it to database
-
-    Args:
-        year (str): year to scrape data from and write data to
-    """
-    if type(year) is not str:
-        raise TypeError("year is not a string")
-    
-    df = webScraper(year)
-    p1 = Process(target=writeStatsToDb('Zscore', calcZscore(df), year))
-    p1.start()
-    p2 = Process(target=writeStatsToDb('Mean', calcMean(df), year))
-    p2.start()
-    p3 = Process(writeStatsToDb('Std', calcStd(df), year))
-    p3.start()
-    p1.join()
-    p2.join()
-    p3.join()
-
-def calcRank(df: pd.DataFrame, col: pd.Series) -> pd.DataFrame:
-    """Calculates rank percentiles for dataframe given to it
-
-    Args:
-        df (pd.DataFrame): scraped data dataframe to be calculated on
-
-    Returns:
-        pd.DataFrame: 2d dataframe with z-scores from original dataframe
-    """
-    if type(df) is not pd.DataFrame:
-        raise TypeError("df is not a dataframe")
-    
-
-    df2 = df.rank(numeric_only=True, pct=True).round(3)
-    df2.insert(0, col.name, col)
-    return df2
 
 def calcPercentile(df: pd.DataFrame, col: pd.Series) -> pd.DataFrame:
     """Calculates percentiles for dataframe given to it. 
@@ -106,22 +66,6 @@ def calcPercentile(df: pd.DataFrame, col: pd.Series) -> pd.DataFrame:
     df2.insert(0, col.name, col)
     return df2
 
-def calcZscore(df: pd.DataFrame, col: pd.Series) -> pd.DataFrame:
-    """Calculates z-scores for dataframe given to it
-
-    Args:
-        df (pd.DataFrame): scraped data dataframe to be calculated on
-
-    Returns:
-        pd.DataFrame: 2d dataframe with z-scores from original dataframe
-    """
-    if type(df) is not pd.DataFrame:
-        raise TypeError("df is not a dataframe")
-    
-    df2 = df.apply(zscore).round(3)
-    df2.insert(0, col.name, col)
-    return df2
-
 def calcMean(df: pd.DataFrame) -> pd.DataFrame:
     """Calculates mean of fields for dataframe given to it
 
@@ -135,21 +79,6 @@ def calcMean(df: pd.DataFrame) -> pd.DataFrame:
         raise TypeError("df is not a dataframe")
 
     df2 = df.mean(numeric_only=True).to_frame().transpose().round(3)
-    return df2
-
-def calcStd(df: pd.DataFrame) -> pd.DataFrame:
-    """Calculates standard deviation of fields for dataframe given to it
-
-    Args:
-        df (pd.DataFrame): scraped data dataframe to be calculated on
-
-    Returns:
-        pd.DataFrame: 1d dataframe with standard deviation from original dataframe
-    """
-    if type(df) is not pd.DataFrame:
-        raise TypeError("df is not a dataframe")
-
-    df2 = df.std(numeric_only=True).to_frame().transpose().round(3)
     return df2
 
 def writeStatsToDb(stat: str, df: pd.DataFrame, year: str):
