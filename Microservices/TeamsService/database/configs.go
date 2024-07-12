72ae2fdc1f1d058733c4cf984032e3e7e9e4e052
Parathan <parathan243@gmail.com>
2024-07-12 19:53:56 -0400

    intialize service and database connection

---
+++ Microservices/TeamsService/database/configs.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(EnvMongoURI()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB")
+	return client
+}
+
+func EnvMongoURI() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return os.Getenv("MONGO_URI")
+}
+
+var Mongo_Client *mongo.Client
--- Microservices/TeamsService/main.go
+++ Microservices/TeamsService/main.go
@@ -1,7 +1,42 @@
 package main
 
-import teamspb "teams-service/proto"
+import (
+	"context"
+	"log"
+	"net"
+	"teams-service/database"
+	teamspb "teams-service/proto"
+
+	"google.golang.org/grpc"
+)
 
 type server struct {
 	teamspb.UnimplementedTeamsServiceServer
+}
+
+func (*server) GetTwoTeams(ctx context.Context, req *teamspb.TwoTeamsRequest) (*teamspb.TwoTeamsResponse, error) {
+	return &teamspb.TwoTeamsResponse{}, nil
+}
+
+func main() {
+	log.Println("Teams Service")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Println("ERROR:", err.Error())
+	}
+
+	database.Mongo_Client = database.ConnectDB()
+
+	defer database.Mongo_Client.Disconnect(context.Background())
+
+	s := grpc.NewServer()
+	teamspb.RegisterTeamsServiceServer(s, &server{})
+
+	log.Printf("Server started at %v", lis.Addr().String())
+
+	err = s.Serve(lis)
+	if err != nil {
+		log.Println("ERROR:", err.Error())
+	}
 }
