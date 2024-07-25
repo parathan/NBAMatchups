cd7261f49d61eb4d5b039dbfc90e8fb9392e84d3
Parathan <parathan243@gmail.com>
2024-07-25 14:25:49 -0400

    removed ml and mobile app and other vscode related directories

--- MobileApp/readme.md
+++
@@ -1,2 +0,0 @@
-`npm install` to install dependencies
-`npx expo start` to start mobile app. Need expo app on your phone for this to run.
--- MobileApp/src/constants/years.js
+++
@@ -1,8 +0,0 @@
-// The years used in the data currently in the matchups page
-export const years = [
-    "2019",
-    "2020",
-    "2021",
-    "2022",
-    "2023"
-]
