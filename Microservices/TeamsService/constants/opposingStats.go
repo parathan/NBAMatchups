a615b5abad27a29bdfbb1632470097cadd99a86d
Parathan <parathan243@gmail.com>
2024-08-04 13:34:03 -0400

    change opposing stats from a function to a variable

--- Microservices/TeamsService/constants/opposingStats.go
+++ Microservices/TeamsService/constants/opposingStats.go
@@ -1,37 +1,90 @@
 package constants
 
-func opposingStats() [][2]string {
-	originalStats := []string{
-		"fg", "fga", "fg_pct", "fg3", "fg3a", "fg3_pct", "fg2", "fg2a", "fg2_pct", "ft", "fta", "ft_pct",
-		"orb", "drb", "trb", "ast", "stl", "blk", "tov", "pf", "pts",
-	}
-	shootingStats := []string{
-		"avg_dist", "pct_fga_fg2a", "pct_fga_00_03", "pct_fga_03_10", "pct_fga_10_16", "pct_fga_16_xx",
-		"pct_fga_fg3a", "fg_pct_00_03", "fg_pct_03_10", "fg_pct_10_16", "fg_pct_16_xx", "pct_ast_fg2",
-		"pct_ast_fg3", "pct_fga_dunk", "fg_dunk", "fg_layup", "pct_fg3a_corner", "fg3_pct_corner",
-	}
-	var opposingStats [][2]string
-
-	for _, field := range originalStats {
-		opposingStats = append(opposingStats, [2]string{field, "opp_" + field})
-		opposingStats = append(opposingStats, [2]string{"opp_" + field, field})
-	}
-	for _, field := range shootingStats {
-		opposingStats = append(opposingStats, [2]string{field, "opp_" + field})
-		opposingStats = append(opposingStats, [2]string{"opp_" + field, field})
-	}
-	extraStats := [][2]string{
-		{"efg_pct", "opp_efg_pct"},
-		{"tov_pct", "opp_tov_pct"},
-		{"orb_pct", "drb_pct"},
-		{"ft_rate", "opp_ft_rate"},
-		{"opp_efg_pct", "efg_pct"},
-		{"opp_tov_pct", "tov_pct"},
-		{"drb_pct", "orb_pct"},
-		{"opp_ft_rate", "ft_rate"},
-	}
-
-	opposingStats = append(opposingStats, extraStats...)
-
-	return opposingStats
+var OpposingStats = [][2]string{
+	{"fg", "opp_fg"},
+	{"opp_fg", "fg"},
+	{"fga", "opp_fga"},
+	{"opp_fga", "fga"},
+	{"fg_pct", "opp_fg_pct"},
+	{"opp_fg_pct", "fg_pct"},
+	{"fg3", "opp_fg3"},
+	{"opp_fg3", "fg3"},
+	{"fg3a", "opp_fg3a"},
+	{"opp_fg3a", "fg3a"},
+	{"fg3_pct", "opp_fg3_pct"},
+	{"opp_fg3_pct", "fg3_pct"},
+	{"fg2", "opp_fg2"},
+	{"opp_fg2", "fg2"},
+	{"fg2a", "opp_fg2a"},
+	{"opp_fg2a", "fg2a"},
+	{"fg2_pct", "opp_fg2_pct"},
+	{"opp_fg2_pct", "fg2_pct"},
+	{"ft", "opp_ft"},
+	{"opp_ft", "ft"},
+	{"fta", "opp_fta"},
+	{"opp_fta", "fta"},
+	{"ft_pct", "opp_ft_pct"},
+	{"opp_ft_pct", "ft_pct"},
+	{"orb", "opp_orb"},
+	{"opp_orb", "orb"},
+	{"drb", "opp_drb"},
+	{"opp_drb", "drb"},
+	{"trb", "opp_trb"},
+	{"opp_trb", "trb"},
+	{"ast", "opp_ast"},
+	{"opp_ast", "ast"},
+	{"stl", "opp_stl"},
+	{"opp_stl", "stl"},
+	{"blk", "opp_blk"},
+	{"opp_blk", "blk"},
+	{"tov", "opp_tov"},
+	{"opp_tov", "tov"},
+	{"pf", "opp_pf"},
+	{"opp_pf", "pf"},
+	{"pts", "opp_pts"},
+	{"opp_pts", "pts"},
+	{"avg_dist", "opp_avg_dist"},
+	{"opp_avg_dist", "avg_dist"},
+	{"pct_fga_fg2a", "opp_pct_fga_fg2a"},
+	{"opp_pct_fga_fg2a", "pct_fga_fg2a"},
+	{"pct_fga_00_03", "opp_pct_fga_00_03"},
+	{"opp_pct_fga_00_03", "pct_fga_00_03"},
+	{"pct_fga_03_10", "opp_pct_fga_03_10"},
+	{"opp_pct_fga_03_10", "pct_fga_03_10"},
+	{"pct_fga_10_16", "opp_pct_fga_10_16"},
+	{"opp_pct_fga_10_16", "pct_fga_10_16"},
+	{"pct_fga_16_xx", "opp_pct_fga_16_xx"},
+	{"opp_pct_fga_16_xx", "pct_fga_16_xx"},
+	{"pct_fga_fg3a", "opp_pct_fga_fg3a"},
+	{"opp_pct_fga_fg3a", "pct_fga_fg3a"},
+	{"fg_pct_00_03", "opp_fg_pct_00_03"},
+	{"opp_fg_pct_00_03", "fg_pct_00_03"},
+	{"fg_pct_03_10", "opp_fg_pct_03_10"},
+	{"opp_fg_pct_03_10", "fg_pct_03_10"},
+	{"fg_pct_10_16", "opp_fg_pct_10_16"},
+	{"opp_fg_pct_10_16", "fg_pct_10_16"},
+	{"fg_pct_16_xx", "opp_fg_pct_16_xx"},
+	{"opp_fg_pct_16_xx", "fg_pct_16_xx"},
+	{"pct_ast_fg2", "opp_pct_ast_fg2"},
+	{"opp_pct_ast_fg2", "pct_ast_fg2"},
+	{"pct_ast_fg3", "opp_pct_ast_fg3"},
+	{"opp_pct_ast_fg3", "pct_ast_fg3"},
+	{"pct_fga_dunk", "opp_pct_fga_dunk"},
+	{"opp_pct_fga_dunk", "pct_fga_dunk"},
+	{"fg_dunk", "opp_fg_dunk"},
+	{"opp_fg_dunk", "fg_dunk"},
+	{"fg_layup", "opp_fg_layup"},
+	{"opp_fg_layup", "fg_layup"},
+	{"pct_fg3a_corner", "opp_pct_fg3a_corner"},
+	{"opp_pct_fg3a_corner", "pct_fg3a_corner"},
+	{"fg3_pct_corner", "opp_fg3_pct_corner"},
+	{"opp_fg3_pct_corner", "fg3_pct_corner"},
+	{"efg_pct", "opp_efg_pct"},
+	{"tov_pct", "opp_tov_pct"},
+	{"orb_pct", "drb_pct"},
+	{"ft_rate", "opp_ft_rate"},
+	{"opp_efg_pct", "efg_pct"},
+	{"opp_tov_pct", "tov_pct"},
+	{"drb_pct", "orb_pct"},
+	{"opp_ft_rate", "ft_rate"},
 }
