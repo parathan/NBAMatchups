dc14e266fb9e652b5b79bb60a6a9d325e5859fbb
Parathan <parathan243@gmail.com>
2024-08-21 12:46:46 -0400

    adjust colours in dashboard

---
+++ WebApp/webapp/src/constants/colours.ts
@@ -0,0 +1,18 @@
+const colours = {
+    SECONDARY_COLOUR: '#444444',
+    BACKGROUND_COLOUR: '#222222',
+    NAVBAR_COLOUR: '#444444',
+    HEADER_FONT_COLOUR: '#ffffff',
+    NAVBAR_FONT_COLOUR: '#dddddd',
+    P_FONT_COLOUR: '#dddddd',
+    TERTIARY_COLOUR: '#333333',
+    SELECTED_FONT_COLOUR: '#a1a1a1',
+    FONT_COLOUR: '#dddddd',
+    SECONDARY_BACKGROUND_COLOUR: '#333333',
+    MENU_FONT_COLOUR: '#c5c5c5',
+
+    RED: "rgb(255, 99, 132)",
+    BLUE: "rgb(53, 162, 235)"
+}
+
+export default colours;
