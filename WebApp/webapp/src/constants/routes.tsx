4175d95d239e17130300354fd7133f40eb7f281d
Parathan <parathan243@gmail.com>
2024-09-12 12:24:32 -0400

    add microservices routes

--- WebApp/webapp/src/constants/routes.tsx
+++ WebApp/webapp/src/constants/routes.tsx
@@ -5,3 +5,8 @@ export const avgData = '/teams';
 export const allTeamsCached = '/teams/allTeamsCached';
 export const orderedPerentileCached = '/teams/cachedOrderedPercentile';
 export const pred_LR = '/ml/LR_pred/teams';
+
+// These constants are for the API routes that are used within the webapp by the microservices
+export const allTeamsMicroservice = '/api/v1/teams/twoteamscached';
+export const orderedPerentileMicroservice = '/api/v1/teams/twoteamsorderedcached';
+export const predictionMicroservice = '/api/v1/prediction/cached';
