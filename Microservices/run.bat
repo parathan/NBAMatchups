1631637933ae9fb4443324fe257c6790801ae795
Parathan <parathan243@gmail.com>
2024-09-12 11:58:21 -0400

    update proto files and bat scripts

--- Microservices/run.bat
+++ Microservices/run.bat
@@ -2,7 +2,7 @@
 
 start cmd /c "cd TeamsService && go run main.go"
 
-start cmd /c "cd predictions && python grpc_server.py"
+start cmd /c "cd predictions && py grpc_server.py"
 
 start cmd /c "cd api-gateway && go run main.go"
 
