a18f62c72dd747aa24abf6675b2cfa17ea9f2689
Parathan <parathan243@gmail.com>
2024-09-12 11:59:55 -0400

    remove unnecessary logs

--- Microservices/api-gateway/controller/predictionController.go
+++ Microservices/api-gateway/controller/predictionController.go
@@ -26,16 +26,12 @@ func PredictController(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Print("predictController called2")
-
 	// Decode the request body into a TwoTeamsRequest
 	var reqBody requests.PredictRequest
 	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
 		http.Error(w, constants.INVALID_JSON_BODY, http.StatusBadRequest)
 		return
 	}
-
-	log.Print("predictController called3")
 
 	// Create a request for the teams service
 	req := &predictions.PredictionRequest{
@@ -44,22 +40,15 @@ func PredictController(w http.ResponseWriter, r *http.Request) {
 		Year:  reqBody.Year,
 	}
 
-	log.Print("predictController called4")
-	log.Print(req)
 	// Get the response from the teams service
 	res, err := predictionsClient.Predict(context.Background(), req)
-	log.Print(res)
-	log.Print(err)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	log.Print("predictController called5")
-
 	// Set the Content-Type header to JSON and encode response to JSON and write it to http.ResponseWriter
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(res)
-	log.Print("predictController called6")
 
 }
