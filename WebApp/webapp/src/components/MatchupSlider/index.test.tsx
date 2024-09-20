f855ea8c5e2a1e72ae783a774cd235fce13d66a2
Parathan <parathan243@gmail.com>
2024-09-19 23:20:04 -0400

    remake tests for matchup slider component

--- WebApp/webapp/src/components/MatchupSlider/index.test.tsx
+++ WebApp/webapp/src/components/MatchupSlider/index.test.tsx
@@ -1,48 +1,39 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 import MatchupSlider from '.';
+import { MatchupData } from '../../interfaces/MatchupData';
+import { MemoryRouter } from 'react-router-dom';
 
-describe('MatchupSlider', () => {
-    const defaultProps = {
-        team1_percentile: 40,
-        team2_op_percentile: 60,
-        field1: 'fg3',
-        field2: 'opp_fg3',
-        team1_trad: 50,
-        team2_op_trad: 60,
-        mean1: 55,
-        mean2: 65,
-        abs_percentile_difference: 0.2,
-        trad_difference: 10,
-        percentile_difference: 20
-    };
+describe('MatchupSlider Component', () => {
+  const mockProps: MatchupData = {
+    team1_percentile: 0.7,
+    team2_op_percentile: 0.5,
+    percentile_difference: 0.2,
+    field1: 'field1',
+    field2: 'field2',
+    team1_trad: 30,
+    team2_op_trad: 25,
+    trad_difference: 5,
+    mean1: 50,
+    mean2: 45,
+    abs_percentile_difference: 0.2,
+  };
 
-    it('renders component with correct state', async () => {
-        render(<MatchupSlider {...defaultProps}/>)
+  test('renders correctly with given props', async () => {
+    render(
+      <MemoryRouter>
+        <MatchupSlider {...mockProps} />
+      </MemoryRouter>
+    );
 
-        const field1 = await screen.findByText('3-Point Field Goals');
-        expect(field1).toBeInTheDocument();
+    // Check that the titles are correctly rendered
+    const fieldHeader = await screen.findByText('field1 vs field2');
+    expect(fieldHeader).toBeInTheDocument();
 
-        const field2 = await screen.findByText('Opponent 3-Point Field Goals');
-        expect(field2).toBeInTheDocument();
-
-        const teamStat1 = await screen.findByText('Team Stat: 50');
-        expect(teamStat1).toBeInTheDocument();
-
-        const teamStat2 = await screen.findByText('Team Stat: 60');
-        expect(teamStat2).toBeInTheDocument();
-
-        const average1 = await screen.findByText('League Average: 55');
-        expect(average1).toBeInTheDocument();
-
-        const average2 = await screen.findByText('League Average: 65');
-        expect(average2).toBeInTheDocument();
-        // expect(screen.getByText('Field 1')).toBeInTheDocument();
-        // expect(screen.getByText('Field 2')).toBeInTheDocument();
-        // expect(screen.getByText('Team Stat: 50')).toBeInTheDocument(); // Assuming initial team stats are 50
-        // expect(screen.getByText('Team Stat: 60')).toBeInTheDocument(); // Assuming initial team stats are 60
-        // expect(screen.getByText('League Average: 55')).toBeInTheDocument();
-        // expect(screen.getByText('League Average: 65')).toBeInTheDocument();
-    })
+    // Check that the team stats and league averages are displayed
+    expect(screen.getByText('Team Stat: 30')).toBeInTheDocument();
+    expect(screen.getByText('League Avg: 50')).toBeInTheDocument();
+    expect(screen.getByText('Team Stat: 25')).toBeInTheDocument();
+    expect(screen.getByText('League Avg: 45')).toBeInTheDocument();
+  });
 })
