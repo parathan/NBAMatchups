20b2b9a92e4b70d05c86b458f5517a2965168721
Parathan <parathan243@gmail.com>
2024-09-24 22:17:03 -0400

    update tests

--- WebApp/webapp/src/pages/Dashboard/index.test.tsx
+++ WebApp/webapp/src/pages/Dashboard/index.test.tsx
@@ -1,10 +1,9 @@
 // import renderer from 'react-test-renderer'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import Dashboard from '.'
 import { MemoryRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
 import axios from 'axios'
-import { statsArray } from '../../constants/statArray'
 import { allTeamsMockData } from '../../constants/mockData'
 
 jest.mock('react-chartjs-2', () => ({ Line: () => null })) // mocks chart in dashboard
@@ -27,51 +26,9 @@ describe('Dashboard Page elements render correctly', () => {
                 <Dashboard />
             </MemoryRouter>
         );
-        const headerArray = await screen.findAllByText(/Dashboard/i);
+        const headerArray = await waitFor(() => screen.findAllByText(/Dashboard/i));
         expect(headerArray.length).toBe(2); // One in the page and one in navbar
     });
-
-    it('renders team dropdown', async () => {
-        render(
-            <MemoryRouter>
-                <Dashboard />
-            </MemoryRouter>
-        );
-        const teamDropdown = await screen.findByText(/Pick a Team/i);
-        expect(teamDropdown).toBeInTheDocument();
-    });
-
-    it('renders field dropdown', async () => {
-        render(
-            <MemoryRouter>
-                <Dashboard />
-            </MemoryRouter>
-        );
-        const fieldDropdown = await screen.findByText(/Pick a Field/i);
-        expect(fieldDropdown).toBeInTheDocument();
-    });
-
-    it('correct number of team dropdowns exist', async () => {
-        render(
-            <MemoryRouter>
-                <Dashboard />
-            </MemoryRouter>
-        );
-
-        const teamDropdowns = await screen.findAllByTestId('team-options')
-        expect(teamDropdowns.length).toBe(30); // 30 teams
-    })
-
-    it('correct number of field dropdowns exist', async () => {
-        render(
-            <MemoryRouter>
-                <Dashboard />
-            </MemoryRouter>
-        );
-
-        const fieldDropdowns = await screen.findAllByTestId('field-options');
-        expect(fieldDropdowns.length).toBe(statsArray.length)
-    })
 })
 
 //TODO #7
