35a7dda9575c29bf6267d5613cff7eac640ecc33
Parathan <parathan243@gmail.com>
2024-10-23 22:29:17 -0400

    update deprecated code

--- Microservices/api-gateway/config/config.go
+++ Microservices/api-gateway/config/config.go
@@ -6,6 +6,7 @@ import (
 	predictions "github.com/parathan/NBAMatchups/Microservices/predictions/prediction-service"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 // CreateTeamsGrpcClient creates a new gRPC client connection to the Teams Service.
@@ -13,17 +14,17 @@ import (
 // The address parameter specifies the address of the Teams Service.
 // Returns a TeamsServiceClient instance and an error if the connection fails.
 func CreateTeamsGrpcClient(address string) (teamspb.TeamsServiceClient, error) {
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		return nil, err
-	}
-	return teamspb.NewTeamsServiceClient(conn), nil
+    conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+    if err != nil {
+        return nil, err
+    }
+    return teamspb.NewTeamsServiceClient(conn), nil
 }
 
 func CreatePredictionsGrpcClient(address string) (predictions.PredictionServiceClient, error) {
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		return nil, err
-	}
-	return predictions.NewPredictionServiceClient(conn), nil
+    conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+    if err != nil {
+        return nil, err
+    }
+    return predictions.NewPredictionServiceClient(conn), nil
 }
