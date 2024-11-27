c06ce8587bb44333a65e10d16e7d72b8306260da
Parathan <parathan243@gmail.com>
2024-11-26 22:48:56 -0500

    update middleware documentation

--- Microservices/api-gateway/middleware/cors.go
+++ Microservices/api-gateway/middleware/cors.go
@@ -2,7 +2,10 @@ package middleware
 
 import "net/http"
 
-// CORS middleware to add headers for cross-origin requests.
+// CorsMiddleware is a middleware function that adds CORS headers to HTTP responses.
+// It allows requests from "http://localhost:3000" and supports the methods GET, POST, PUT, DELETE, and OPTIONS.
+// The middleware handles preflight OPTIONS requests by immediately responding with an OK status.
+// It then calls the next handler in the chain for all other requests.
 func CorsMiddleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         // Add CORS headers
