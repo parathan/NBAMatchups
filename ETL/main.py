09acdf468dafa18b6e33d4069a7ef282bcbfb52c
Parathan <parathan243@gmail.com>
2025-04-15 23:02:22 -0400

    update years

--- ETL/main.py
+++ ETL/main.py
@@ -1,7 +1,8 @@
 from businessLogic import *
 
 def main():
-    for i in range(2019, 2024):
+    for i in range(2019, 2026):
+        print("year: ", i)
         writeAllStats(str(i))
         # calcData(str(i))
         # rawData(str(i))
--- Microservices/TeamsService/constants/years.go
+++ Microservices/TeamsService/constants/years.go
@@ -1,6 +1,6 @@
 package constants
 
 const (
-	MAX_YEAR = 2023
+	MAX_YEAR = 2025
 	MIN_YEAR = 2019
 )
